d0a346902e4495041d73d529d3fbea3c24e84013
geofire <64675996+geofire@users.noreply.github.com>
2024-12-20 23:19:25 +1100

    Changed unit testing to pytest.

--- tests/test_lint.py
+++ tests/test_lint.py
@@ -1,38 +1,27 @@
-import unittest
-
+import pytest
 import honeybee.lint as lint
 
 
-class TestLint(unittest.TestCase):
-    def test_defines_variable(self):
-        self.assertFalse(lint.defines_variable({"type": ""}))
-        self.assertFalse(lint.defines_variable({"type": "note"}))
-        self.assertTrue(lint.defines_variable({"type": "integer"}))
+def test_defines_variable():
+    assert not lint.defines_variable({"type": ""})
+    assert not lint.defines_variable({"type": "note"})
+    assert lint.defines_variable({"type": "integer"})
 
-    def test_defines_select(self):
-        self.assertFalse(lint.defines_select({"type": ""}))
-        self.assertFalse(lint.defines_select({"type": "integer"}))
-        self.assertTrue(lint.defines_select({"type": "select_one foo"}))
-        self.assertTrue(lint.defines_select({"type": "select_multiple foo"}))
 
-    def test_extract_list_names(self):
-        self.assertTupleEqual(
-            lint.extract_list_names({"type": "integer"}),
-            tuple())
-        self.assertTupleEqual(
-            lint.extract_list_names({"type": "select_one foo"}),
-            ("foo",))
-        self.assertTupleEqual(
-            lint.extract_list_names({"type": "select_one foo or_other"}),
-            ("foo",))
-        self.assertTupleEqual(
-            lint.extract_list_names({"type": "select_one foo bar"}),
-            ("foo", "bar"))
+def test_defines_select():
+    assert not lint.defines_select({"type": ""})
+    assert not lint.defines_select({"type": "integer"})
+    assert lint.defines_select({"type": "select_one foo"})
+    assert lint.defines_select({"type": "select_multiple foo"})
 
-    def test_extract_variables(self):
-        self.assertTupleEqual(
-            lint.extract_variables("foo barbaz"),
-            tuple())
-        self.assertTupleEqual(
-            lint.extract_variables("foo ${bar}${baz}"),
-            ("bar", "baz"))
+
+def test_extract_list_names():
+    assert lint.extract_list_names({"type": "integer"}) == tuple()
+    assert lint.extract_list_names({"type": "select_one foo"}) == ("foo",)
+    assert lint.extract_list_names({"type": "select_one foo or_other"}) == ("foo",)
+    assert lint.extract_list_names({"type": "select_one foo bar"}) == ("foo", "bar")
+
+
+def test_extract_variables():
+    assert lint.extract_variables("foo barbaz") == tuple()
+    assert lint.extract_variables("foo ${bar}${baz}") == ("bar", "baz")
